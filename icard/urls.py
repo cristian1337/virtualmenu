f9873c65e6d7bc4ed59579d158dfc703b7c12343
Cristian Alberto Osorio <cristian.osorio@cerok.com>
2022-10-22 13:10:58 -0500

    requiretments

--- icard/urls.py
+++ icard/urls.py
@@ -54,6 +54,6 @@ urlpatterns = [
     path('api/', include(router_order.urls)),
     path('api/', include(router_payments.urls)),
     path('api/', include(router_delivery.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
